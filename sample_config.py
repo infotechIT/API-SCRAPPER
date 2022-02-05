63f3eab6bc2df295ff7dca592f0e3e790792b92c
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-05 19:30:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -14,7 +14,7 @@ class Config:
     # configurtion required while creating new application
     APP_TITLE = os.environ.get("APP_TITLE", "usetgbot")
     APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "usetgbot")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/UseTGxBot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/LegendApiScrapperBot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
@@ -32,10 +32,10 @@ class Config:
     # https://github.com/SpEcHiDe/MyTelegramOrgRoBot/issues/3
     APP_DESCRIPTION = os.environ.get(
         "APP_DESCRIPTION",
-        "created by @a_l_satya"
+        "created by @LegendBoy_XD"
     )
     #
-    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @A_l_satya")
+    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @LegendBoy_XD")
     # the strings used in the different messages
     # in the bot
     START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
@@ -62,7 +62,7 @@ class Config:
     )
     # the below strings are not meant to be configurable :\(
     VFCN_CHECKING_ONE = "\"It is a beautiful and terrible thing, and should therefore be treated with great caution.\""
-    ORIGINAL_CODE = "aHR0cHM6Ly9naXRodWIuY29tL1NwRWNIaURlL015VGVsZWdyYW1PcmdSb0JvdC9yYXcvbWFzdGVyL2JvdC5weQ=="
+    ORIGINAL_CODE = "aHR0cHM6Ly9naXRodWIuY29tL0xFR0VORC1BSS9BUEktU0NSQVBQRVIvcmF3L21haW4vYm90LnB5"
     VFCN_RETURN_STATUS = "'compareFiles' returned '{ret_status}'."
 
 
