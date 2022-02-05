ccc15dd9257d94fc1c77699a8e7b0c099f1a14c4
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-05 19:15:50 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,13 @@
+class Translation(object):
+    START_TEXT = """Hi!
+I cant help you beacuse you are not authorized 
+contact @The_LegendBoy to use me or he will provide another bot
+remember only he can do this dont spam me if you spam i will not give you reply
+
+So Stop here my friend.. """
+    AFTER_RECVD_CODE_TEXT = """I said just stop here dont spam me cotact @The_LegendBoy To Geg Authorized"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings."
+    CANCELLED_MESG = "I said just stop here dont spam me cotact @The_LegendBoy to use me"
+    IN_VALID_CODE_PVDED = "Plz cotact @The_LegendBoy to use me"
+    IN_VALID_PHNO_PVDED = "Without Authorized Of @The_LegendBoy U cant use me"
