3c56dfa251b32db202c0f1fe49e25b5575c57c2e
InfoTech <infotechtele@gmail.com>
2022-04-16 20:22:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,14 @@
 class Translation(object):
-    START_TEXT =Hi, Sir!
-I can get your Telegram APP ID & API Hash from my.telegram.org !!
-No need to go to website & login there. Just Login with me.
+    START_TEXT ="""Hi Welcome To Telegram Org Bot!
+Thanks To Using Me
+Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
-Just Send me your Telegram Account's Phone Number to start process.
-Phone Number should be in International Format.
-Example: +9141985053855
-
-/start at any stage to re-enter your details
+/start at any stage to re-enter your details"""
     AFTER_RECVD_CODE_TEXT = """I see!
 now please send the Telegram code that you received from Telegram!
 this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/LEGEND-AI/API-SCRAPPER and clicking on the Pink Button
+by opening https://github.com/infotechIT/API-SCRAPPER and clicking on the Pink Button
 
 /start at any stage to re-enter your details"""
 
